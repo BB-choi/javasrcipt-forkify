5d466c308a13b07ea7c63c009903af13c0c45e66
BB <choibobae0809@gmail.com>
2021-09-21 17:08:04 +0900

    Add New feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -129,6 +129,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application 🎈');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -137,5 +141,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearhResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
